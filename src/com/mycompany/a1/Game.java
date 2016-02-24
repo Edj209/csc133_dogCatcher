a166ed8cf81370d79f05a6e3358ef99606c86482
Edj209 <edgarsjv@gmail.com>
2016-02-24 02:25:44 -0800

    created method shells and case statements

--- src/com/mycompany/a1/Game.java
+++ src/com/mycompany/a1/Game.java
@@ -37,8 +37,50 @@ public class Game extends Form{
                         gw.expand();
                         break;
                     case 'c':
+                        gw.contract();
+                        break;
+                    case 's':
+                        gw.scoop();
+                        break;
+                    case 'r':
+                        gw.right();
+                        break;
+                    case 'l':
+                        gw.left();
+                        break;
+                    case 'u':
+                        gw.up();
+                        break;
+                    case 'd':
+                        gw.down();
+                        break;
+                    case 'o':
+                        gw.netToRandomDog();
+                        break;
+                    case 'a':
+                        gw.netToRandomCat();
+                        break;
+                    case 'k':
+                        gw.catCollision();
+                        break;
+                    case 'f':
+                        gw.catDogFight();
+                        break;
+                    case 't':
+                        gw.tick();
+                        break;
+                    case 'p':
+                        gw.points();
+                        break;
+                    case 'm':
+                        gw.map();
+                        break;
+                    case 'q':
+                        gw.quit();
+                        break;
+                    default:
 
-
+                        break;
 
                 }
             }
--- src/com/mycompany/a1/GameWorld.java
+++ src/com/mycompany/a1/GameWorld.java
@@ -15,4 +15,36 @@ public class GameWorld {
 
     }
 
+    //contracts the net
+    public void contract() {}
+
+
+    //scoops up all the animals in the net
+    public void scoop() {}
+
+    public void right() {}
+
+    public void left() {}
+
+    public void up() {}
+
+    public void down(){}
+
+    public void netToRandomDog() {}
+
+    public void netToRandomCat() {}
+
+    public void catCollision() {}
+
+    public void catDogFight() {}
+
+    public void tick() {}
+
+    public void points() {}
+
+    public void map() {}
+
+    public void quit() {}
+
+
 }
